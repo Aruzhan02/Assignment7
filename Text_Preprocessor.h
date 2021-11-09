a2fb632f4450c6277cb8a4359b9c71c7e66df8e3
Aruzhan02 <aaruzhan232@gmail.com>
2021-11-09 23:32:41 +0600

    Commit

--- Text_Preprocessor.h
+++ Text_Preprocessor.h
@@ -7,144 +7,70 @@
 using namespace std;
 
 
-class Text_Preprocessor {
+class Text_Preprocessor{
 
 public:
-
-    string nameTxt;
-
-    int counter = 0;
-    int counter1 = 0;
-    int c = 0;
-
-    Text_Preprocessor(string nameTxt){
-        this->nameTxt = nameTxt;
-    };
-
-    ofstream fout;
-
-    ifstream fin;
-
-
-
-
-    void openFile(){
-
-
-        fin.open(nameTxt);
-
-        fout.open(nameTxt, ofstream::app);
-
-        if (!fin.is_open())
-        {
-            cout << "Файл не открылся..." << endl;
-        }
-        else {
-
-            cout << "Файл открылся!" << endl;
-
-            string str;
-
-            int counterForCounter = 0;
-
-            char ch;
-
-            while(fin.get(ch)){
-
-                counterForCounter++;
-
+    string text2;
+    ifstream textfile;
+    Text_Preprocessor(char *text_name){
+        cout << text_name << endl;
+        textfile.open(text_name, ios::in);
+        char soz;
+        string soz1 = "";
+        string soz2;
+        while(getline(textfile, soz2, '\0')){
+            for(int i = 0; i < soz2.size(); ++i){
+                soz = soz2[i];
+                soz1 += soz;
             }
-
-            setCounter(counterForCounter);
-
-        }
-
-        fin.close();
-        fout.close();
-    }
-
-    void deleteNeNuzhnoe(){
-        fin.open(nameTxt);
-
-        fout.open(nameTxt, ofstream::app);
-
-            string str;
-
-            char ch;
-            fout << endl;
-            while(fin.get(ch)){
-
-                string s;
-
-                if(ch == 44 || ch == 46 || ch == 34 || ch == 58 || ch == 59 || ch == 63 || ch == 33){}
-
-                else if(ch > 64 && ch < 91){
-
-                    ch = ch + 32;
-
-                    fout << ch;
-
-                    cout << ch;
-
-                }
-
-                else{
-
-                    fout << ch;
-
-                    cout << ch;
-
-                }
-
-                counter1++;
-
-                if(getCounter() == counter1){
-                    break;
-                }
-
-            }
-
-        fin.close();
-        fout.close();
-        }
-
-    void deletePrev(){
-        fin.open(nameTxt);
-
-        fout.open(nameTxt, ofstream::in);
-
-        string s;
-
-        char ch;
-
-        while(fin.get(ch)){
-
-            ch = 0;
-
-                fout << ch;
-
-
-            c++;
-
-            if(getCounter() + 11 == c){
+            if(textfile.eof()){
                 break;
             }
-
         }
-
-        fin.close();
-        fout.close();
+        this->text2 = soz1;
+    }
+    ~Text_Preprocessor(){//destructor
+        textfile.close();
+    }
+    void preprocess_text(){
+        string result = "";
+        string file = this->text2;
+        for(int i = 0; i < file.size(); ++i){
+            if(file[i] != ',' && file[i] != '"' && file[i] != '.' && file[i] != ';' && file[i] != ':' && file[i] != '!' && file[i] != '?'){
+                if(isalpha(file[i])) {
+                    result += tolower(file[i]);
+                }
+                else{
+                    result += file[i];
+                }
+            }
+        }
+        string birdene1 = "";
+        string answer = "";
+        for(int i = 0; i < result.size(); ++i){
+            if(result[i] == ' '){
+                if(birdene1 != "a" && birdene1 != "an" && birdene1 != "the" && birdene1 != "in" && birdene1 != "on"
+                   && birdene1 != "at" && birdene1 != "since" && birdene1 != "for" && birdene1 != "before" && birdene1 != "ago" && birdene1 != "to" && birdene1 != "by"){
+                    answer += birdene1;
+                    answer += " ";
+                }
+                birdene1.clear();
+            }
+            else{
+                birdene1 += result[i];
+            }
+        }
+        if(birdene1 != "a" && birdene1 != "an" && birdene1 != "the" && birdene1 != "in" && birdene1 != "on"
+           && birdene1 != "at" && birdene1 != "since" && birdene1 != "for" && birdene1 != "before" && birdene1 != "ago" && birdene1 != "to" && birdene1 != "by"){
+            answer += birdene1;
+            answer += " ";
+        }
+        birdene1.clear();
+        this->text2 = answer;
     }
 
-    int getCounter() const {
-        return counter;
-    }
+};
 
-    void setCounter(int counter) {
-        Text_Preprocessor::counter = counter;
-    }
-
-}; // Записать данные в текстовый файл, думаю я показал что я это более менее умею в первом таске, дедлайн горит)
 
 
 #endif //CLIONPROJECTS_TEXT_PREPROCESSOR_H
--- main.cpp
+++ main.cpp
@@ -3,10 +3,38 @@
 
 using namespace std;
 int main() {
-
-    Text_Preprocessor n("Text_1.txt");
-
-    n.openFile();
-    n.deleteNeNuzhnoe();
-    n.deletePrev();
+    //main class
+    char array_text[1000];
+    cin >> array_text;
+    Text_Preprocessor san(array_text);
+    san.preprocess_text();
+    int index = san.search_word("a");
+    if(index == -1){
+        cout << "Word not found!";
+    }
+    else{
+        cout << index << endl;
+    }
+    ifstream file;
+    file.open("C:/Users/aaruz/CLionProjects/ass7/Task2.txt");
+    ofstream outfile1 ("Thread_1.txt");
+    ofstream outfile2 ("Thread_2.txt");
+    double *array1 = new double[1002];
+    double ave = 0;
+    for(int i = 0; i < 100; ++i){
+        file >> array1[i];
+        ave += array1[i];
+    }
+    ave /= 100;
+    for(int i = 0; i < 100; ++i){
+        if(array1[i] - ave > 0){
+            outfile1 << array1[i] << endl;
+        }
+        else{
+            outfile2 << array1[i] << endl;
+        }
+    }
+    outfile1.close();
+    outfile2.close();
+    return 0;
 }
